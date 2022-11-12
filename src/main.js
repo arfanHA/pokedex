6aefeef38078b6cadc2bef1371f061d9347d0d35
Ipul-FE <45265088+arfanHA@users.noreply.github.com>
2022-11-12 23:36:06 +0800

    setting up main file

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import axios from "axios";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(router);
+app.use(store);
+app.use(ElementPlusIconsVue);
+app.mount("#app");
+
+axios.defaults.baseURL = "https://pokeapi.co/api/v2";
