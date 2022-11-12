16fc8514f8b585faa3883fee1f411288b2d4ea56
Ipul-FE <45265088+arfanHA@users.noreply.github.com>
2022-11-12 23:39:52 +0800

    + styling to app file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/favorite">Favorite</router-link>
   </div>
   <router-view />
 </template>
@@ -26,5 +26,56 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+
+.infinite-list-wrapper {
+  height: 64vh;
+  text-align: left;
+}
+.infinite-list-wrapper .list {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+.infinite-list-wrapper .list-item {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  height: 50px;
+  background: var(--el-color-primary-light-9);
+}
+.infinite-list-wrapper .list-item + .list-item {
+  margin-top: 10px;
+}
+
+td.value {
+  display: flex;
+  color: orange;
+}
+
+td.title {
+  display: flex;
+}
+
+.detail-btn {
+  margin-right: 5px;
+}
+
+.demonstration {
+  display: block;
+  width: 100%;
+  font-size: 10px;
+  margin-bottom: 20px;
+}
+
+.el-radio-group {
+  margin: 5px;
+}
+
+.fav-btn,
+.detail-btn {
+  margin-left: auto;
 }
 </style>
